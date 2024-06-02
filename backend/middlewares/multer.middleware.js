12e28241b8253b442ccfaa24b3f4dc140edec168
Om Patil <114414115+ompatil7@users.noreply.github.com>
2024-06-02 18:06:07 +0530

    Update multer.middleware.js

--- backend/middlewares/multer.middleware.js
+++ backend/middlewares/multer.middleware.js
@@ -1,16 +1,34 @@
-import multer from "multer";
+const multer = require("multer");
+const path = require("path");
+const cloudinary = require("cloudinary").v2;
+const { uploadOnCloudinary } = require("./utils/cloudinary");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, path.join(__dirname, "public", "temp"));
   },
   filename: function (req, file, cb) {
-    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    // cb(null, file.fieldname + "-" + uniqueSuffix);
     cb(null, file.originalname);
   },
 });
 
-export const upload = multer({
-  storage,
-});
+const upload = multer({ storage });
+
+module.exports = { upload };
+
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp");
+//   },
+//   filename: function (req, file, cb) {
+//     // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     // cb(null, file.fieldname + "-" + uniqueSuffix);
+//     cb(null, file.originalname);
+//   },
+// });
+
+// export const upload = multer({
+//   storage,
+// });
